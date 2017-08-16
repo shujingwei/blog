7643c7780393cf40964a0dd636cd87ea0273125f
shujingwei <shujingwei@meituan.com>
2017-08-16 11:32:09 +0800

    webhook.js console

--- webhooks.js
+++ webhooks.js
@@ -5,6 +5,7 @@ http.createServer(function (req, res) {
     // 该路径与WebHooks中的路径部分需要完全匹配，实现简易的授权认证。
     if(req.url === '/webhooks/push/blog'){
         // 如果url匹配，表示认证通过，则执行 sh ./deploy.sh
+	console.log("pull...");
         exec('sh ./deploy.sh')
     }
     res.end()
