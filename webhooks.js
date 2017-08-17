3923d21d67a1cbe194ad6271f980ffad9ad00852
shujingwei <shujingwei@meituan.com>
2017-08-18 00:47:44 +0800

    remove console

--- webhooks.js
+++ webhooks.js
@@ -2,7 +2,7 @@ var http = require('http')
 var exec = require('child_process').exec
 
 http.createServer(function (req, res) {
-    console.log(req);
+    // console.log(req);
     // 该路径与WebHooks中的路径部分需要完全匹配，实现简易的授权认证。
     if(req.url === '/webhooks/push/blog'){
         // 如果url匹配，表示认证通过，则执行 sh ./deploy.sh
